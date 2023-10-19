d8a88b6c3141ab2a9806da1f2badbba94a91b994
kosmo <kosmosensei@gmail.com>
2023-10-19 16:31:36 +0700

    Initial commit

---
+++ src/main/java/pro/sky/tgbotcatshelter/TgbotCatShelterApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.tgbotcatshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgbotCatShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgbotCatShelterApplication.class, args);
+	}
+
+}
