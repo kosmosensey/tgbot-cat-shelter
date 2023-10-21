0c39c1dc9b17ec4f35001274055897c71629032f
action0709 <action0709@mail.ru>
2023-10-21 16:28:13 +0400

    тест2

--- src/main/java/pro/sky/tgbotcatshelter/TgbotCatShelterApplication.java
+++ src/main/java/pro/sky/tgbotcatshelter/TgbotCatShelterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TgbotCatShelterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TgbotCatShelterApplication.class, args);
 	}
