43597fe3f2bd102db3bb262b3c2a900d245cdb1f
ranss@rambler.ru <ranss@rambler.ru>
2023-11-05 22:51:56 +0300

    Внесен в Listener метод выведения сообщений после кнопок, внесены текстовые данные в класс Messages

--- src/main/java/pro/sky/tgbotcatshelter/constants/Messages.java
+++ src/main/java/pro/sky/tgbotcatshelter/constants/Messages.java
@@ -26,5 +26,68 @@ public class Messages {
             Дорогой усыновитель, мы заметили, что ты заполняешь отчет не так подробно, как необходимо.
             Пожалуйста, подойди ответственнее к этому занятию.
             В противном случае волонтеры приюта будут обязаны самолично проверять условия содержания животного""";
+
+    public static final String RULES_FOR_DATING_CATS = """
+            Правила знакомства с животными""";
+
+    public static final String RULES_FOR_DATING_DOGS = """
+        Правила знакомства с животными""";
+
+
+    public static final String DOCUMENTS_TO_PICK_UP_A_ANIMALS = """
+            Список документов, необходимых для того, чтобы взять животное из приюта""";
+
+    public static final String RECOMMENDATIONS_FOR_TRANSPORTING_A_CAT = """
+           Список рекомендаций по транспортировке кошки""";
+
+
+    public static final String RECOMMENDATIONS_FOR_TRANSPORTING_A_DOG = """
+           Список рекомендаций по транспортировке кошки""";
+
+    public static final String ARRANGE_A_HOME_FOR_A_KITTEN = """
+          Список рекомендаций по обустройству дома для котенка""";
+
+    public static final String ARRANGE_A_HOME_FOR_A_PUPPY = """
+           Список рекомендаций по обустройству дома для щенка""";
+
+    public static final String SET_UP_A_HOME_FOR_AN_ADULT_CAT  = """
+           Список рекомендаций по обустройству дома для взрослой кошки""";
+
+    public static final String SET_UP_A_HOME_FOR_AN_ADULT_DOG  = """
+           Список рекомендаций по обустройству дома для взрослой собаки""";
+
+    public static final String ARRANGE_A_HOME_FOR_A_DISABLED_ANIMAL = """
+           Список рекомендаций по обустройству дома для животного с ограниченными возможностями""";
+
+    public static final String REASONS_FOR_REFUSAL = """
+           Список причин, почему могут отказать и не дать забрать животное из приюта""";
+
+    public static final String TIPS_FOR_INITIAL_COMMUNICATION_WITH_A_DOG = """
+           Советы кинолога по первичному общению с собакой""";
+
+    public static final String RECOMMENDATIONS_FROM_DOG_HANDLERS = """
+           Рекомендации проверенных кинологов""";
+    public static final String INFORMATION_ABOUT_THE_SHELTER_CATS = """
+        О нашем приюте для кошек""";
+
+    public static final String INFORMATION_ABOUT_THE_SHELTER_DOGS = """
+        О нашем приюте для собак""";
+
+
+    public static final String SCHEDULE_ADDRESS_DIRECTIONS_CATS = """
+        Расписание работы приюта кошек и адрес, схему проезда""";
+
+    public static final String SCHEDULE_ADDRESS_DIRECTIONS_DOGS = """
+        Расписание работы приюта для собак и адрес, схему проезда""";
+
+
+    public static final String SECURITY_INFORMATION_FOR_ISSUING_A_PASS = """
+        Контактные данные охраны для оформления пропуска на машину""";
+
+
+    public static final String SAFETY_PRECAUTIONS = """
+        Рекомендации о технике безопасности на территории приюта""";
+
+
     public static final String GREETINGS_GUEST7 = "";
 }
--- src/main/java/pro/sky/tgbotcatshelter/listener/TgBotCatShelterUpdatesListener.java
+++ src/main/java/pro/sky/tgbotcatshelter/listener/TgBotCatShelterUpdatesListener.java
@@ -2,6 +2,7 @@ package pro.sky.tgbotcatshelter.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
@@ -53,6 +54,7 @@ public final class TgBotCatShelterUpdatesListener implements UpdatesListener {
                 // Если это обратный вызов (например, нажатие кнопки)
                 text = update.callbackQuery().data();
                 chatId = update.callbackQuery().message().chat().id();
+                createMessageOut(update);
             } else {
                 // Игнорируем неизвестные типы обновлений
                 return;
@@ -68,7 +70,7 @@ public final class TgBotCatShelterUpdatesListener implements UpdatesListener {
                 markup.addRow(
                         new InlineKeyboardButton("Приют для кошек").callbackData("/c1"));
                 markup.addRow(
-                        new InlineKeyboardButton("Приют для собак").callbackData("/с2"));
+                        new InlineKeyboardButton("Приют для собак").callbackData("/d1"));
 
                 SendMessage send2 = new SendMessage(chatId, "Выберите интересующий вас приют:")
                         .replyMarkup(markup);
@@ -83,35 +85,214 @@ public final class TgBotCatShelterUpdatesListener implements UpdatesListener {
                 markup.addRow(
                         new InlineKeyboardButton("Прислать отчет о питомце").callbackData("/сat3"));
                 markup.addRow(
-                        new InlineKeyboardButton("Позвать волонтера приюта кошек").callbackData("/cat4"));
+                        new InlineKeyboardButton("Позвать волонтера приюта").callbackData("/cat4"));
 
                 SendMessage send = new SendMessage(chatId, "Выберите один из вариантов:")
                         .replyMarkup(markup);
                 // Отправляем сообщение send
                 telegramBot.execute(send);
-            } else if (text.equalsIgnoreCase("/с2")) {
+            } else if (text.equalsIgnoreCase("/d1")) {
                 InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
                 markup.addRow(
-                        new InlineKeyboardButton("Узнать информацию о приюте для собак").callbackData("/d1"));
+                        new InlineKeyboardButton("Узнать информацию о приюте для собак").callbackData("/dog1"));
                 markup.addRow(
-                        new InlineKeyboardButton("Как взять животное из приюта собак").callbackData("/d2"));
+                        new InlineKeyboardButton("Как взять животное из приюта собак").callbackData("/dog2"));
                 markup.addRow(
-                        new InlineKeyboardButton("Прислать отчет о питомце").callbackData("/d3"));
+                        new InlineKeyboardButton("Прислать отчет о питомце").callbackData("/dog3"));
                 markup.addRow(
-                        new InlineKeyboardButton("Позвать волонтера приюта собак").callbackData("/d4"));
+                        new InlineKeyboardButton("Позвать волонтера приюта").callbackData("/dog4"));
 
                 SendMessage send = new SendMessage(chatId, "Выберите один из вариантов:")
                         .replyMarkup(markup);
                 // Отправляем сообщение send
                 telegramBot.execute(send);
-            } else {
-                SendMessage send1 = new SendMessage(chatId, "команда не определена");
-                // Отправляем сообщение send1 об ошибке
-                telegramBot.execute(send1);
+            } else if (text.equalsIgnoreCase("/сat1")) {
+                InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+                markup.addRow(
+                        new InlineKeyboardButton("О нашем приюте кошек").callbackData("/сat11"));
+                markup.addRow(
+                        new InlineKeyboardButton("Расписание работы приюта кошек и адрес, схему проезда")
+                                .callbackData("/сat12"));
+                markup.addRow(
+                        new InlineKeyboardButton("Контактные данные охраны для оформления пропуска на машину").callbackData("/сat13"));
+                markup.addRow(
+                        new InlineKeyboardButton("Рекомендации о технике безопасности на территории приюта").callbackData("/сat14"));
+                markup.addRow(
+                        new InlineKeyboardButton("Принять и записать контактные данные для связи").callbackData("/сat15"));
+                markup.addRow(
+                        new InlineKeyboardButton("Позвать волонтера приюта").callbackData("/сat16"));
+
+                SendMessage send = new SendMessage(chatId, "Выберите один из вариантов:")
+                        .replyMarkup(markup);
+                // Отправляем сообщение send
+
+                telegramBot.execute(send);
+            } else if (text.equalsIgnoreCase("/dog1")) {
+                InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+                markup.addRow(
+                        new InlineKeyboardButton("О нашем приюте для собак").callbackData("/dog11"));
+                markup.addRow(
+                        new InlineKeyboardButton("Расписание работы приюта и адрес, схему проезда")
+                                .callbackData("/dog12"));
+                markup.addRow(
+                        new InlineKeyboardButton("Контактные данные охраны для оформления пропуска на машину").callbackData("/dog13"));
+                markup.addRow(
+                        new InlineKeyboardButton("Рекомендации о технике безопасности на территории приюта").callbackData("/dog14"));
+                markup.addRow(
+                        new InlineKeyboardButton("Принять и записать контактные данные для связи").callbackData("/dog15"));
+                markup.addRow(
+                        new InlineKeyboardButton("Позвать волонтера приюта").callbackData("/dog16"));
+
+                SendMessage send = new SendMessage(chatId, "Выберите один из вариантов:")
+                        .replyMarkup(markup);
+                // Отправляем сообщение send
+
+                telegramBot.execute(send);
+            } else if (text.equalsIgnoreCase("/сat2")) {
+                InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+                markup.addRow(
+                        new InlineKeyboardButton("Правила знакомства с животным").callbackData("/сat21"));
+
+                markup.addRow(
+                        new InlineKeyboardButton("Список документов, необходимых для того, чтобы взять животное из приюта")
+                                .callbackData("/сat22"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по транспортировке кошки/кота").callbackData("/сat23"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по обустройству дома для котенка").callbackData("/сat24"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по обустройству дома для взрослого животного").callbackData("/сat25"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по обустройству дома для животного с ограниченными возможностями").callbackData("/сat26"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список причин, почему могут отказать и не дать забрать кошку из приюта").callbackData("/сat27"));
+                markup.addRow(
+                        new InlineKeyboardButton("Принять и записать контактные данные для связи").callbackData("/сat28"));
+                markup.addRow(
+                        new InlineKeyboardButton("Позвать волонтера приюта кошек").callbackData("/сat29"));
+
+                SendMessage send = new SendMessage(chatId, "Выберите один из вариантов:")
+                        .replyMarkup(markup);
+                // Отправляем сообщение send
+
+                telegramBot.execute(send);
+            } else if (text.equalsIgnoreCase("/dog2")) {
+                InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+                markup.addRow(
+                        new InlineKeyboardButton("Правила знакомства с животным").callbackData("/dog21"));
+
+                markup.addRow(
+                        new InlineKeyboardButton("Список документов, необходимых для того, чтобы взять собаку из приюта")
+                                .callbackData("/dog22"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по транспортировке собаки").callbackData("/dog23"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по обустройству дома для щенка").callbackData("/dog24"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по обустройству дома для взрослого животного").callbackData("/dog25"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список рекомендаций по обустройству дома для животного с ограниченными возможностями").callbackData("/dog26"));
+                markup.addRow(
+                        new InlineKeyboardButton("Список причин, почему могут отказать и не дать забрать собаку из приюта").callbackData("/dog27"));
+                markup.addRow(
+                        new InlineKeyboardButton("Cоветы кинолога по первичному общению с собакой").callbackData("/dog28"));
+                markup.addRow(
+                        new InlineKeyboardButton("Рекомендации проверенных кинологов").callbackData("/dog29"));
+                markup.addRow(
+                        new InlineKeyboardButton("Принять и записать контактные данные для связи").callbackData("/dog210"));
+                markup.addRow(
+                        new InlineKeyboardButton("Позвать волонтера").callbackData("/dog211"));
+
+                SendMessage send = new SendMessage(chatId, "Выберите один из вариантов:")
+                        .replyMarkup(markup);
+                // Отправляем сообщение send
+
+                telegramBot.execute(send);
             }
         });
         // Возвращаем подтверждение обработанных обновлений
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+    private void createMessageOut(Update update) {
+        CallbackQuery callbackQuery = update.callbackQuery();
+        if (callbackQuery != null) {
+            String data = callbackQuery.data();
+            switch (data) {
+                case "/dog210" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), GET_CONTACT_INFORMATION));
+                case "/dog211" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), CALL_VOLUNTEER));
+                case "/сat3" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SEND_PETS_REPORT));
+                case "/dog3" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SEND_PETS_REPORT));
+                case "/cat4" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), CALL_VOLUNTEER));
+                case "/dog4" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), CALL_VOLUNTEER));
+                case "/сat21" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), RULES_FOR_DATING_CATS));
+                case "/сat22" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), DOCUMENTS_TO_PICK_UP_A_ANIMALS));
+                case "/сat23" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), RECOMMENDATIONS_FOR_TRANSPORTING_A_CAT));
+                case "/сat24" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), ARRANGE_A_HOME_FOR_A_KITTEN));
+                case "/сat25" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SET_UP_A_HOME_FOR_AN_ADULT_CAT));
+                case "/сat26" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), ARRANGE_A_HOME_FOR_A_DISABLED_ANIMAL));
+                case "/сat27" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), REASONS_FOR_REFUSAL));
+                case "/сat28" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), GET_CONTACT_INFORMATION));
+                case "/сat29" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), CALL_VOLUNTEER));
+                case "/dog21" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), RULES_FOR_DATING_DOGS));
+                case "/dog22" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), DOCUMENTS_TO_PICK_UP_A_ANIMALS));
+                case "/dog23" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), RECOMMENDATIONS_FOR_TRANSPORTING_A_DOG));
+                case "/dog24" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), ARRANGE_A_HOME_FOR_A_PUPPY));
+                case "/dog25" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SET_UP_A_HOME_FOR_AN_ADULT_DOG ));
+                case "/dog26" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), ARRANGE_A_HOME_FOR_A_DISABLED_ANIMAL));
+                case "/dog27" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), REASONS_FOR_REFUSAL));
+                case "/dog28" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), TIPS_FOR_INITIAL_COMMUNICATION_WITH_A_DOG));
+                case "/dog29" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), RECOMMENDATIONS_FROM_DOG_HANDLERS));
+                case "/сat11" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), INFORMATION_ABOUT_THE_SHELTER_CATS));
+                case "/сat12" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SCHEDULE_ADDRESS_DIRECTIONS_CATS));
+                case "/сat13" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SECURITY_INFORMATION_FOR_ISSUING_A_PASS));
+                case "/сat14" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SAFETY_PRECAUTIONS));
+                case "/сat15" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), GET_CONTACT_INFORMATION));
+                case "/сat16" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), CALL_VOLUNTEER));
+                case "/dog11" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), INFORMATION_ABOUT_THE_SHELTER_DOGS));
+                case "/dog12" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SCHEDULE_ADDRESS_DIRECTIONS_DOGS));
+                case "/dog13" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SECURITY_INFORMATION_FOR_ISSUING_A_PASS));
+                case "/dog14" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), SAFETY_PRECAUTIONS));
+                case "/dog15" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), GET_CONTACT_INFORMATION));
+                case "/dog16" ->
+                        telegramBot.execute(new SendMessage(update.callbackQuery().from().id(), CALL_VOLUNTEER));
+            }
+        }
+    }
+
 }
 
